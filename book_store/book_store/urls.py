322cda1582ed142e010fb6ffe13738636eee9232
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-12-22 21:12:14 +0200

    model relationships practice

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+# ivan
+# test@test.com
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('book_outlet.urls')),
