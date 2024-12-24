40575b7a86d0e83aadbff89fb3750ea32897603b
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-12-24 15:03:26 +0200

    Added models to represent blogs

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "tags", 'date')
+    list_display = ("title", "date", "author",)
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+
+
+
+# ivan
+# admin
