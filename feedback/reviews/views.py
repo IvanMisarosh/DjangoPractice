235ef6a0dd3c49d2fcdd6be31f930a81829d48df
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-12-24 17:32:21 +0200

    forms practice

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+
+from .forms import ReviewForm
 
 # Create your views here.
+class ReviewView(View):
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect('/thank_you')
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
